7be7847393422e5ddf8a1e0870d694a2b6e97dfd
Naomi Rivkin <nomi420@gmail.com>
2025-03-02 13:13:58 +0200

    fixing double click on the same card case

--- scripts.js
+++ scripts.js
@@ -1,32 +1,38 @@
 const cards = document.querySelectorAll(".card");
 
-let isFlippedCard = false;
+let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
 
 function flipCard() {
   if (lockBoard) return;
+  if (this === firstCard) return; // in case of double click
   this.classList.toggle("flip");
-  if (!isFlippedCard) {
-    isFlippedCard = true;
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
     firstCard = this;
   } else {
-    isFlippedCard = false;
     secondCard = this;
 
     if (firstCard.dataset.framework == secondCard.dataset.framework) {
       firstCard.removeEventListener("click", flipCard);
       secondCard.removeEventListener("click", flipCard);
+      resetBoard();
     } else {
       lockBoard = true;
       setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
 
-        lockBoard = false;
+        resetBoard();
       }, 1500);
     }
   }
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
