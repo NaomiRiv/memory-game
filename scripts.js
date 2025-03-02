17b30a8975a102665bd5876cc517ef62a2d474ae
Naomi Rivkin <nomi420@gmail.com>
2025-03-02 13:04:03 +0200

    adding card flip

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll(".card");
 
+let isFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle("flip");
+  if (!isFlippedCard) {
+    isFlippedCard = true;
+    firstCard = this;
+  } else {
+    isFlippedCard = false;
+    secondCard = this;
+
+    if (firstCard.dataset.framework == secondCard.dataset.framework) {
+      firstCard.removeEventListener("click", flipCard);
+      secondCard.removeEventListener("click", flipCard);
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+      }, 1500);
+    }
+  }
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
